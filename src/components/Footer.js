7bc6f44c498fb6b914161f98c9e9d2833eedb0df
Manuel Di Leve <manueldilevem@gmail.com>
2025-04-18 12:40:26 +0200

    Aggiunti componenti Header, Nav, Main e Footer con logo e meta tag

---
+++ src/components/Footer.js
@@ -0,0 +1,10 @@
+function Footer() {
+    return (
+        <footer>
+            <p>Ⓒ  2025 Little Lemon - Tutti i diritti riservati</p>
+            <p>Contattaci: info@littlelemon.it</p>
+        </footer>
+    );
+}
+
+export default Footer;
---
+++ src/components/Main.js
@@ -0,0 +1,25 @@
+function Main() {
+    return (
+        <main>
+            <section>
+                <h2>Ortona</h2>
+                <p>Ristorante con cucina tradizionale, tipica del posto e dove ogni tuodesiderio verrà realizzato</p>
+                <button>Reserve a table</button>
+            </section>
+
+            <section>
+                <h2>this weeks specials!</h2>
+                <article>
+                    <h3>Greek salad</h3>
+                    <p>The famous greek salad of crispy lettuce, peppers, olives and our Chicago style feta cheese, garnished with crunchy garlic and rosemary croutons.</p>
+                </article>
+                <article>
+                    <h3>Bruschetta</h3>
+                    <p>Our bruchetta is made from grilled bread that has been smeared wtih garlic and seasoned with salt and olive oil.</p>
+                </article>
+            </section>
+        </main>
+    );
+}
+
+export default Main;
